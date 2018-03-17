f29d0ca419a9e76bfe24e7e48732523dfaf63a36
JasonDodd511 <jason.dodd@cambent.com>
2018-03-17 00:46:39 -0400

    Adding support for Divi 3 visual builder

--- obdiy-divi-enhancements.php
+++ obdiy-divi-enhancements.php
@@ -26,6 +26,7 @@ function fmpm_add_post_types($post_types) {
 	return $post_types;
 }
 add_filter('et_builder_post_types', 'fmpm_add_post_types');
+add_filter('et_fb_post_types', 'fmpm_add_post_types');
 
 /* Add Divi Custom Post Settings box */
 function fmpm_add_meta_boxes() {
